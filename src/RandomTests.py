8e0f3a3dee9007b7ed0a163fa0fa54332614db67
vi_acosta <55991392+VitorAcosta@users.noreply.github.com>
2022-04-04 20:19:41 -0300

    Atualização da classe principal
    Incorporação de módulos de apoio

--- src/RandomTests.py
+++ src/RandomTests.py
@@ -1,23 +1,34 @@
-
+from modules.KeyConverter import KeyConverter
 
 class RandomTests():
   def __init__(self):
-    FILENAME = "src/keys.txt"
+    self.FILENAME = "src/keys.txt"
     self.keys = []
-    self.read_keys(FILENAME)
+    self.kc = KeyConverter()
 
-  def read_keys(self, filename):
+  def read_keys(self, filename, return_type_expected):
     with open(filename) as rf:
       for line in rf.readlines():
-        self.keys.append(line)
+        treated_line = line.replace("\"","").replace("'","").replace("\n","")
+        if treated_line != '':
+          if return_type_expected == "binary":
+            converted_line = self.kc.convert_hex_to_binary(treated_line)
+          else:
+            converted_line = treated_line
+
+          self.keys.append(converted_line)
+      
+      return self.keys
 
   def monobit_test(self):
     pass
 
   def poker_test(self):
     pass
-  
+
   def run_test(self, long_run = False):
     print(long_run)
 
 rt = RandomTests()
+a = rt.read_keys("src/keys.txt","binary")
+print(a[-1])
